93369527552f7ebab7a05edd409b353b19577529
dukim <dukim@bnpinnovation.com>
2019-02-21 13:30:29 +0900

    sleuth-zipkin 연동

--- zipkin-server/src/main/java/com/bnpinnovation/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/bnpinnovation/zipkinserver/ZipkinServerApplication.java
@@ -2,8 +2,12 @@ package com.bnpinnovation.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
+@EnableEurekaClient
 public class ZipkinServerApplication {
 
     public static void main(String[] args) {

0e7d7fbe352e9286cdbe34117edf0c165acb5ed4
dukim <dukim@bnpinnovation.com>
2019-02-21 12:21:30 +0900

    zipkin library added

---
+++ zipkin-server/src/main/java/com/bnpinnovation/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.bnpinnovation.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+}
